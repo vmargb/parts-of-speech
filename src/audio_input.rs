45b7d4f9368ce604423691b1800c3d80786bf4db
vmargb <sharificles@gmail.com>
2026-09-02 22:57:17 +0100

    fixed wrong hardware channel count

--- src/audio_input.rs
+++ src/audio_input.rs
@@ -24,8 +24,8 @@ pub fn start_input_stream(
     {
         let mut rec = recorder.lock().unwrap();
         rec.project.sample_rate = hardware_sample_rate;
-        rec.project.channels = hardware_channels;
-        // println!("Hardware: {}Hz, {} channel(s)", hardware_sample_rate, hardware_channels);
+        rec.project.channels = 1; // always use mono
+        println!("Hardware: {}Hz, {} channel(s)", hardware_sample_rate, hardware_channels);
     }
 
     let stream = device.build_input_stream(
